949b13c8641a28d1314072aec405bcebe25af8c0
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-15 15:34:13 -0400

    Created first sample questions with takes user input

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title of Project',
+        message: 'What is the Title of the Project (REQUIRED)',
+        validate: projectTitle => {
+            if(projectTitle) {
+                console.log(questions)
+                return true;
+            } else {
+                console.log('Please Enter a Project Title')
+                return false
+            }
+        }
+    }
+];
 
+const promptUser = () => {
+    return inquirer.prompt(questions);
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log(questions)
+}
 
 // Function call to initialize app
 init();
+promptUser();
